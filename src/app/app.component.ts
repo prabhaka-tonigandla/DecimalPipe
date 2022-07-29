dee185fee8b62553bfc9e3f7a9c15b195b95dc91
prabhaka-tonigandla <prabhakar.tonigandla@icloud.com>
2022-07-29 10:17:41 +0530

    Updated for two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata';
 import * as format from './shared/helper.component';
 import { DecimalPipe } from '@angular/common/src/pipes';
+import { formatNumber } from '@angular/common';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -14,6 +15,7 @@ export class AppComponent implements OnInit {
   { "price": 5678901234567 },
 ];
 endPB: number;
+startPB: string;
 constructor(){
     this.item.forEach(element => {
       element.price = format.formatCurrency(element.price);
@@ -22,7 +24,14 @@ constructor(){
 
 }
 ngOnInit(): void {
-this.endPB = "12.7676";
+this.endPB = 12.7676;
+this.startPB = "45.34534534";
+}
+Save(): void{
+  //console.log('test');
+  console.log(formatNumber(this.endPB,"en-US", "1.2-2"));
+  // console.log(this.endPB);
+  // console.log(this.startPB);
 }
 }
 
