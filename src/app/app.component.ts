36161c3e3fafb1c3dd90e43dd17cd056a21deb4c
prabhaka-tonigandla <prabhakar.tonigandla@icloud.com>
2022-07-29 12:08:41 +0530

    format number for all properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { OnInit } from '@angular/core/src/metadata';
 import * as format from './shared/helper.component';
 import { DecimalPipe } from '@angular/common/src/pipes';
 import { formatNumber } from '@angular/common';
+import { LoanData } from './shared/Loandetails';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -17,21 +18,46 @@ export class AppComponent implements OnInit {
 endPB: number;
 startPB: string;
 constructor(){
-    this.item.forEach(element => {
-      element.price = format.formatCurrency(element.price);
-    });
-
-
+ 
 }
 ngOnInit(): void {
-this.endPB = 12.7676;
-this.startPB = "45.34534534";
+
+
+
 }
 Save(): void{
-  //console.log('test');
-  console.log(formatNumber(this.endPB,"en-US", "1.2-2"));
+  console.log('test');
+  //console.log(formatNumber(this.endPB,"en-US", "1.2-2"));
+  //this.startPB=formatNumber(this.startPB,"en-US", "1.2-2");
   // console.log(this.endPB);
-  // console.log(this.startPB);
+   //console.log(`two way binding value ${this.startPB}`);
+   let obj = new LoanData();
+obj.claim=10;
+obj.endPB=123.344;
+obj.loanNumber=123.788;
+// Object.keys(a).forEach(key => formatNumber(Number(key),"en-US", "1.2-2"));
+// Object.keys(a).forEach(key =>{
+//    console.log(`${Reflect.get(a,key)}`);
+// Object.keys(obj).forEach(e => {
+//   //console.log(`key=${e}  value=${a[e]}`);
+//  // this[a.e]=formatNumber(a[e],"en-US", "1.2-2");
+
+//  Object.keys(obj).forEach(key => {
+//   obj[key] = 'redacted';
+// });
+
+// });
+Object.keys(obj).forEach(key => {
+  
+  obj[key] =     formatNumber(obj[key],"en-US", "1.2-2");
+
+});
+Object.keys(obj).forEach(e => console.log(`key=${e}  value=${obj[e]}`));
+// console.log(a.loanNumber);
+
+// const keys = Object.keys(a);
+//     const values = keys.map(key => `${key}: ${Reflect.get(a,key)}`)
+//     console.log(values)
 }
 }
 
---
+++ src/app/shared/Loandetails.ts
@@ -0,0 +1,10 @@
+export class LoanData
+{
+
+  loanNumber : Number;
+  endPB: Number;
+  remit: Number;
+  claim: Number;
+  gain: Number;
+  loss: Number;
+}
